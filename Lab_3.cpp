7efff7f406152e8fa225911d6ba731c58e3e0c69
dendum <dendumych@gmail.com>
2021-11-01 00:33:04 +0200

    vgfgAdd new Lab 3 code

--- Lab_3.cpp
+++ Lab_3.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    const int z = 1000;
-    char str[z];
-    char new_str[z];
-    int y = 0;
+    const int size = 100;
+    char str[size];
+    char new_str[size];
+    int count = 0;
     
     cout << "Enter your line:" << endl;
+<<<<<<< HEAD
     cin.getline(str,z);
+=======
+    cin.getline(str,size);
+>>>>>>> 682a9df (Add new Lab 3 code)
 
-    for ( int x = 0; x < z; x++)
+    for ( int x = 0; x < size; x++)
     {
         if (str[x] != ' ')
         {
-            new_str[y] = str[x];
-            y++;
+            new_str[count] = str[x];
+            count++;
         }
     }
     
-    cout << "Your new line:" << endl;
-    cout << new_str << endl;
+    cout << "Your new line:" << endl << new_str << endl;
 }
